8f75f163265d702f6f100b5fb56d6122ee5111c3
Chris-0307 <135561620+Chris-0307@users.noreply.github.com>
2026-09-15 18:26:09 -0600

    Configuracion multirregion

---
+++ proyecto1/configure.sql
@@ -0,0 +1,16 @@
+
+-- Configuracion de regiones de la base de datos
+
+-- Region principal del sistema.
+ALTER DATABASE ti4601
+SET PRIMARY REGION "tienda-a";
+
+-- Regiones adicionales.
+ALTER DATABASE ti4601
+ADD REGION IF NOT EXISTS "tienda-b";
+
+ALTER DATABASE ti4601
+ADD REGION IF NOT EXISTS "cd-central";
+
+-- Verificacion de las regiones configuradas.
+SHOW REGIONS FROM DATABASE ti4601;
